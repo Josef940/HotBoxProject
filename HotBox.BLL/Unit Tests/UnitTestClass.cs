b33a0ed40b387f9048198853074e067c4891b4fb
JosefPC\JosefG <josef940@gmail.com>
2016-11-05 03:31:42 +0100

    Unit tests added

--- HotBox.BLL/Unit Tests/UnitTestClass.cs
+++ HotBox.BLL/Unit Tests/UnitTestClass.cs
@@ -17,21 +17,44 @@ namespace HotBox.BLL.Unit_Tests
         string XMLString = Properties.Resources.XMLString;
         Facade facade = Facade.Instance;
        
+        private HttpResponseMessage CreateResponseMessage()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent(XMLString);
+            return response;
+        }
+
         [Test]
         public void Serializes_XML_To_HotBox_Test()
         {
-            HttpResponseMessage validresponse = new HttpResponseMessage();
-            validresponse.Content = new StringContent(XMLString);
+            HttpResponseMessage validresponse = CreateResponseMessage();
             TrendProject validhotboxdata = facade.GetDataLogic().XMLSerializeToHotbox(validresponse);
             // 71 is the CncAddress attribute value in the XMLString.txt file
             Assert.AreEqual(71, validhotboxdata.Site.CncAddress);
 
             // Returns null if response cannot be serialized
-            HttpResponseMessage invalidresponse = new HttpResponseMessage();
-            invalidresponse.Content = new StringContent("You cannot serialize this");
+            HttpResponseMessage invalidresponse = CreateResponseMessage();
             TrendProject invalidhotboxdata = facade.GetDataLogic().XMLSerializeToHotbox(validresponse);
             Assert.AreEqual(null,invalidhotboxdata);
 
+        }
+        [Test]
+        public void Retrieving_Values_From_HotBox_Test()
+        {
+            HttpResponseMessage response = CreateResponseMessage();
+            TrendProject hotboxdata = facade.GetDataLogic().XMLSerializeToHotbox(response);
+            List<HotBoxValues> hbvalues = facade.GetDataLogic().GetHotBoxValues(hotboxdata);
+
+            Assert.AreEqual(19638480,hbvalues[0].Value);
+        }
+        [Test]
+        public void Get_Modules_From_HotBox_Test()
+        {
+            HttpResponseMessage response = CreateResponseMessage();
+            TrendProject hotboxdata = facade.GetDataLogic().XMLSerializeToHotbox(response);
+            List<Module> modules = facade.GetDataLogic().GetModules(hotboxdata);
+
+            Assert.AreEqual("S100",modules[0].Name);
         }
     }
 }
